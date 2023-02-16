1229caa3abc1c7a074717ee3e4fffc44956472be
Meylis Matiyev <meylismatiyev@gmail.com>
2023-02-16 09:18:37 +0100

    added dependencies

--- src/main/java/com/meylism/Main.java
+++ src/main/java/com/meylism/Main.java
@@ -1,7 +1,13 @@
 package com.meylism;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logger logger = LogManager.getLogger(Main.class);
+        logger.debug("debug log");
+        logger.warn("warn log");
     }
+
 }
