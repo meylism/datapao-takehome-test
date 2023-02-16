97162b11a61067b02d2a5039f442ef5259c35a7a
Meylis Matiyev <meylismatiyev@gmail.com>
2023-02-16 09:53:17 +0100

    Implementing data-structures

--- src/main/java/com/meylism/ds/IMDBMovie.java
+++ src/main/java/com/meylism/ds/IMDBMovie.java
@@ -1,4 +1,35 @@
 package com.meylism.ds;
 
+import com.google.common.base.Preconditions;
+import lombok.Getter;
+
 public class IMDBMovie {
+    @Getter
+    private String title;
+    @Getter
+    private Double rating;
+    @Getter
+    private Integer numRating;
+    @Getter
+    private Integer numOscar;
+
+    public void setTitle(String title) {
+        Preconditions.checkArgument(title != null && title.length() > 0, "Title should exist");
+        this.title = title;
+    }
+
+    public void setRating(Double rating) {
+        Preconditions.checkArgument(rating >= 0.0);
+        this.rating = rating;
+    }
+
+    public void setNumRating(Integer numRating) {
+        Preconditions.checkArgument(numRating >= 0);
+        this.numRating = numRating;
+    }
+
+    public void setNumOscar(Integer numOscar) {
+        Preconditions.checkArgument(numOscar >= 0);
+        this.numOscar = numOscar;
+    }
 }
--- src/main/java/com/meylism/ds/ScrapeResult.java
+++ src/main/java/com/meylism/ds/ScrapeResult.java
@@ -1,4 +1,24 @@
 package com.meylism.ds;
 
+import com.google.common.base.Preconditions;
+import lombok.Getter;
+
+/**
+ * Class that encompasses all data of a scraping event.
+ */
 public class ScrapeResult {
+    @Getter
+    private long scrapeTime;
+    @Getter
+    private Object data;
+
+    public void setScrapeTime(long scrapeTime) {
+        Preconditions.checkArgument(scrapeTime >= 0, "Scrape time should be non-negative");
+        this.scrapeTime = scrapeTime;
+    }
+
+    public void setData(Object data) {
+        Preconditions.checkArgument(data != null, "Data object cannot be null");
+        this.data = data;
+    }
 }
